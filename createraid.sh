cd5c06663bfc9bfc5a5a034371a5c6fa8ce11971
d4rkgh0m <120186195+d4rkgh0m@users.noreply.github.com>
2023-11-28 17:29:47 +0300

    createraid.sh

--- createraid.sh
+++
@@ -1,7 +0,0 @@
-!#/bin/bash
-
-sudo mdadm --zero-superblock --force /dev/sd{b,c,d,e,f,g}
-sudo mdadm --create --verbose /dev/md0 -l 10 -n 6 /dev/sd{b,c,d,e,f,g}
-sudo mkdir /etc/mdadm
-sudo echo "DEVICE partitions" > /etc/mdadm/mdadm.conf
-sudo mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf
